09191675698df96a01fc8843fe9252802b3640a6
ignas <ignasi9494@gmail.com>
2022-02-09 16:14:05 +0100

    Print the A or M status, and the percent brightness bar in the BrightControl class

--- src/BrightControl.cpp
+++ src/BrightControl.cpp
@@ -86,28 +86,85 @@ Bright * Bright_Create(void){
 * @Return: -
 *
 ************************************************/
-void Actualize_Window_Bright(Bright* const me, Adafruit_ST7735 *tft)
+void Actualize_Window_Bright(Bright* const me, Adafruit_ST7735 *tft, bool wind_changed)
 {
-    //Clear screen
-    tft->fillScreen(ST77XX_WHITE);
+
     /*
     tft->fillCircle(x_centre, y_centre, rad, ST77XX_RED);
     tft->fillCircle(x_centre, y_centre, rad - thick, ST77XX_WHITE);
     */
-    tft->fillCircle(50, 50, 10, ST77XX_RED);
-    tft->fillCircle(50, 50, 10 - 2, ST77XX_WHITE);
+   
+    if (wind_changed)
+    {
+        //Clear screen
+        tft->fillScreen(ST77XX_WHITE);
+        tft->fillCircle(50, 65, 30, ST77XX_BLACK);
+    }
+    tft->fillCircle(50, 65, 30 - 4, ST77XX_WHITE);
     //Set text cursor
-    tft->setCursor(50, 10);
+    tft->setCursor(40, 52);
     //Set text color
     tft->setTextColor(ST77XX_BLACK);
     //Set text size
-    tft->setTextSize(3);
-    if (me->automatic){
+    tft->setTextSize(4);
+
+    if (me->automatic)
+    {
+        //Print "A" that means automátic
         tft->println("A");
-    }else{
+    }
+    else
+    {
+        //Print "M" that means manual
         tft->println("M");
     }
-    //Print "A" that means automátic
+
+    tft->fillRoundRect(110, 10, 30, 110, 13, ST77XX_WHITE);
+
+
+
+    switch (me->bright_percent)
+    {
+        case PERCENT_0:
+
+            break;
+        
+        case PERCENT_12_5:
+            tft->fillRoundRect(110, 97, 30, 17, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_25:
+            tft->fillRoundRect(110, 89, 30, 25, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_37_5:
+            tft->fillRoundRect(110, 77, 30, 37, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_50:
+            tft->fillRoundRect(110, 64, 30, 50, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_62_5:
+            tft->fillRoundRect(110, 52, 30, 62, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_75:
+            tft->fillRoundRect(110, 39, 30, 75, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_87_5:
+
+            tft->fillRoundRect(110, 27, 30, 87, 10, ST77XX_BLACK);
+            break;
+
+        case PERCENT_100:
+
+            tft->fillRoundRect(110, 14, 30, 100, 10, ST77XX_BLACK);
+            break;
+        
+    }
+    
 }
 
 
@@ -161,7 +218,7 @@ void Motor_Bright_Control(Bright *me, bool up_pressed, bool low_pressed, bool ce
 
     }
 
-    if (wind_changed || center_pressed || low_pressed || up_pressed) Actualize_Window_Bright(me, tft);
+    if (wind_changed || center_pressed || low_pressed || up_pressed) Actualize_Window_Bright(me, tft, wind_changed);
 
     if (last_percent != me->bright_percent){
         me->changed = true;
